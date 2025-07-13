436a9af1dd92980e0302c0122db2b39656a21843
fikriaf <143471086+fikriaf@users.noreply.github.com>
2025-07-13 11:32:29 +0700

    Update LoadingPage.tsx

--- faftech-react/src/components/LoadingPage.tsx
+++ faftech-react/src/components/LoadingPage.tsx
@@ -8,41 +8,36 @@ type LoaderProps = {
 
 const Loader: React.FC<LoaderProps> = ({ text = "" }) => {
   return (
-    <>
-        <div className="w-100 vh-100 d-flex align-items-center justify-content-center">
-          <div className="d-flex flex-column justify-content-center">
-            <StyledWrapper>
-              <div className="honeycomb">
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-                <div />
-              </div>
-            </StyledWrapper>
-            <div className="text-dark mt-5">{text}</div>
+    <div className="position-fixed top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center bg-white">
+      <div className="d-flex flex-column align-items-center text-center">
+        <StyledWrapper>
+          <div className="honeycomb">
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
+            <div />
           </div>
-        </div>
-
-    </>
+        </StyledWrapper>
+        {text && <div className="text-dark mt-4">{text}</div>}
+      </div>
+    </div>
   );
-}
+};
 
 const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
   @-webkit-keyframes honeycomb {
-    0%,
-    20%,
-    80%,
-    100% {
+    0%, 20%, 80%, 100% {
       opacity: 0;
       -webkit-transform: scale(0);
       transform: scale(0);
     }
-
-    30%,
-    70% {
+    30%, 70% {
       opacity: 1;
       -webkit-transform: scale(1);
       transform: scale(1);
@@ -50,17 +45,12 @@ const StyledWrapper = styled.div`
   }
 
   @keyframes honeycomb {
-    0%,
-    20%,
-    80%,
-    100% {
+    0%, 20%, 80%, 100% {
       opacity: 0;
       -webkit-transform: scale(0);
       transform: scale(0);
     }
-
-    30%,
-    70% {
+    30%, 70% {
       opacity: 1;
       -webkit-transform: scale(1);
       transform: scale(1);
@@ -70,13 +60,11 @@ const StyledWrapper = styled.div`
   .honeycomb {
     height: 24px;
     position: relative;
-    width: 56px; /* atau 60px lebih aman */
-  margin: 0 auto; 
-    
+    width: 56px;
+    margin: 0 auto;
   }
 
   .honeycomb div {
-    -webkit-animation: honeycomb 2.1s infinite backwards;
     animation: honeycomb 2.1s infinite backwards;
     background: #242424ff;
     height: 12px;
@@ -85,7 +73,8 @@ const StyledWrapper = styled.div`
     width: 24px;
   }
 
-  .honeycomb div:after, .honeycomb div:before {
+  .honeycomb div:after,
+  .honeycomb div:before {
     content: '';
     border-left: 12px solid transparent;
     border-right: 12px solid transparent;
@@ -105,52 +94,52 @@ const StyledWrapper = styled.div`
   }
 
   .honeycomb div:nth-child(1) {
-    -webkit-animation-delay: 0s;
     animation-delay: 0s;
     left: -28px;
     top: 0;
   }
 
   .honeycomb div:nth-child(2) {
-    -webkit-animation-delay: 0.1s;
     animation-delay: 0.1s;
     left: -14px;
     top: 22px;
   }
 
   .honeycomb div:nth-child(3) {
-    -webkit-animation-delay: 0.2s;
     animation-delay: 0.2s;
     left: 14px;
     top: 22px;
   }
 
   .honeycomb div:nth-child(4) {
-    -webkit-animation-delay: 0.3s;
     animation-delay: 0.3s;
     left: 28px;
     top: 0;
   }
 
   .honeycomb div:nth-child(5) {
-    -webkit-animation-delay: 0.4s;
     animation-delay: 0.4s;
     left: 14px;
     top: -22px;
   }
 
   .honeycomb div:nth-child(6) {
-    -webkit-animation-delay: 0.5s;
     animation-delay: 0.5s;
     left: -14px;
     top: -22px;
   }
 
   .honeycomb div:nth-child(7) {
-    -webkit-animation-delay: 0.6s;
     animation-delay: 0.6s;
     left: 0;
     top: 0;
-  }`;
+  }
+
+  @media (max-width: 768px) {
+    .honeycomb {
+      transform: scale(0.8);
+    }
+  }
+`;
 
 export default Loader;
